1a756d1d9acac2f265b92fc67c22249625d71a2d
Bhanu prakash Mallepoola <sravan8147@gmail.com>
2022-09-27 15:14:41 +0000

    Update 3 files

--- frontend/src/common/components/ui/custom_popup.js
+++ frontend/src/common/components/ui/custom_popup.js
@@ -1,38 +1,43 @@
-import * as React from 'react';
-import Button from '@mui/material/Button';
-import TextField from '@mui/material/TextField';
-import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
-import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material/DialogContentText';
-import DialogTitle from '@mui/material/DialogTitle';
-import DashboardCustomTextField from './dashboard_custom_text_field';
+import * as React from "react";
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+import DashboardCustomTextField from "./dashboard_custom_text_field";
+import { Grid } from "@mui/material";
 
 export default function CustomFormDialog({ isOpen, submitAction }) {
-    const [open, setOpen] = React.useState(false);
+  const [open, setOpen] = React.useState(false);
 
-    const handleClickOpen = () => {
-        setOpen(true);
-    };
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
 
-    const handleClose = () => {
-        setOpen(false);
-    };
+  const handleClose = () => {
+    setOpen(false);
+  };
 
-    return (
-        <div>
-            <Dialog open={isOpen} onClose={handleClose}>
-                <DialogTitle>KPIs Approval</DialogTitle>
-                <DialogContent>
-                    <Grid item md={6}>
-                        
-                    </Grid>
-                </DialogContent>
-                <DialogActions>
-                    <Button onClick={handleClose}>Close</Button>
-                    <Button onClick={(v) => { submitAction(v) }}>Submit</Button>
-                </DialogActions>
-            </Dialog>
-        </div>
-    );
+  return (
+    <div>
+      <Dialog open={isOpen} onClose={handleClose}>
+        <DialogTitle>KPIs Approval</DialogTitle>
+        <DialogContent>
+          <Grid item md={6}></Grid>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Close</Button>
+          <Button
+            onClick={(v) => {
+              submitAction(v);
+            }}
+          >
+            Submit
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
 }
